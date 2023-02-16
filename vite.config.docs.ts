4beb87acea6b3e1c44763d79a9d0f97b7e480360
Thy <uipi@foxmail.com>
2023-02-17 00:06:23 +0800

    #

--- vite.config.docs.ts
+++ vite.config.docs.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/vue2-repl/',
   plugins: [vue()],
   build: {
     target: 'esnext',
